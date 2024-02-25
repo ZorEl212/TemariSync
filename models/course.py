cd06a3b526fa1ff19f20a750432738866f5e0bb1
ZorEl212 <yabsirad212@proton.me>
2024-02-25 15:20:54 +0300

    Models: implement some properties for handling relationships

--- models/course.py
+++ models/course.py
@@ -1,8 +1,18 @@
 from models.base import BaseModel
+import models
 
 class Course(BaseModel):
     """Course Class"""
 
     name = ""
     dept_id = ""
-    
+
+    @property
+    def documents(self):
+        all = models.storage.all()
+        docs = {}
+        for key, values in all.items():
+            if hasattr(values, 'course_id'):
+                if values.cls_name == 'Document' and values.course_id == self.id:
+                    docs[key] = values
+        return docs
--- models/document.py
+++ models/document.py
@@ -1,8 +1,26 @@
 from models.base import BaseModel
-
+import models
+from datetime import datetime
+import shutil
+import os
 class Document(BaseModel):
     title = ""
     category = ""
     course_id = ""
     stud_id = ""
     tags = []
+    filename= ""
+
+    def save(self, file):
+        """Updates time metadata of an instance and saves it on main dictionary"""
+
+        setattr(self, 'updated_at', datetime.now())
+        base, extention = os.path.splitext(os.path.basename(file))
+        self.filename = f'{self.id}{extention}'
+        shutil.copy(file, f'docs/{self.filename}')
+        models.storage.save()
+
+    def get_file(self):
+        extention = os.path.splitext(self.filename)[-1]
+        shutil.copy(f'docs/{self.filename}', f'return/{self.title}{extention}')
+        return "Saved Successfully!"
