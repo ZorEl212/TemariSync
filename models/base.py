d1d180f8733839e83364d4d97aea30734d2ed51a
ZorEl212 <yabsirad212@proton.me>
2024-02-25 15:18:34 +0300

    BaseModel: Fix date time parsing issue. 	Fix issue of datetime objects behaving like strings sometimes by adding some checks 	Documentation: Make some modifications on the documentations, also some misc changes

--- models/base.py
+++ models/base.py
@@ -8,9 +8,11 @@ class BaseModel:
     for other classes"""
 
     def __init__(self, **kwargs):
-        """Init point
-            Args:
-                kwargs: arbirary dict of args"""
+        """Initialization method.
+        
+        Args:
+            kwargs (dict): Arbitrary keyword arguments.
+        """
 
         self.id = str(uuid4())
         self.created_at = datetime.now()
@@ -22,39 +24,36 @@ class BaseModel:
             if key != '__class__':
                 setattr(self, key, value)
         models.storage.new(self)
-        
 
     def __str__(self):
-        """Return dict structure of an instance."""
-
+        """Return string representation of an instance."""
         return f"[{self.cls_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates time metadata of an instance and saves it on main dictionary"""
-
-        setattr(self, 'updated_at', datetime.now())
+        """Updates time metadata of an instance and saves it on main dictionary."""
+        self.updated_at = datetime.now()
         models.storage.save()
 
     @classmethod
     def get_cls_name(cls):
+        """Return the class name."""
         return cls.__name__
 
     @property
     def cls_name(self):
-        """return a string value which is the class name an 
-        instance is created from"""
-
+        """Return the class name an instance is created from."""
         return self.get_cls_name()
 
     def to_dict(self):
         """Returns dictionary representation of an instance more suited for
-            serialzing to JSON file"""
-
+        serializing to JSON file."""
         res = vars(self)
         for key, value in res.items():
             if key in ['created_at', 'updated_at']:
-                res[key] = value.isoformat(timespec='microseconds')
+                if isinstance(value, datetime):
+                    res[key] = value.isoformat(timespec='microseconds')
+                else:
+                    datetime_obj = datetime.strptime(value,  '%Y-%m-%dT%H:%M:%S.%f')
+                    res[key] = datetime_obj.isoformat(timespec='microseconds')
         res['__class__'] = self.cls_name
         return res
-            
-        
