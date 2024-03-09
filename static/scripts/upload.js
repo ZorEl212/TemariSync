e8e54a41ab2d4a5bf580bfdb058849dbf3006f9a
ZorEl212 <yabsirad212@gmail.com>
2024-03-09 13:39:24 +0300

    Frontend: Push edit doc template files       Upload: remove unnecessary content
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- static/scripts/upload.js
+++ static/scripts/upload.js
@@ -6,10 +6,11 @@ $(document).ready(function () {
         const title = $('#title').val();
         const discription = $('#description').val();
         const tags = $('#tags').val();
-        const course = $('#course').val();
+        let course = $('#course').val();
         const coAuthor = $('#coAuthor').val();
         let category;
         const docfile = $('#docfile');
+        const caancel = $('#cancel');
         const user_id = localStorage.getItem('user_id');
         $('input[type="radio"][name="option"]').each(function () {
             if ($(this).is(':checked')) {
@@ -32,11 +33,6 @@ $(document).ready(function () {
             return;
         }
 
-        if (coAuthor.trim() === '') {
-            alert('Please enter a co-author.');
-            return;
-        }
-
         if (!category) {
             alert('Please select a category.');
             return;
@@ -45,28 +41,67 @@ $(document).ready(function () {
             alert('Please enter a description.');
             return;
         }
-        formData.append('title', title);
-        formData.append('description', discription);
-        formData.append('tags', tags);
-        formData.append('course_id', course);
-        formData.append('coAuthor', coAuthor);
-        formData.append('category', category);
-        formData.append('stud_id', localStorage.getItem('user_id'));
-        formData.append('file', docfile[0].files[0]);
+        if ($('#authorsNote').val().trim() === '') {
+            alert(`Please enter Author's note.`);
+            return;
+        }
 
+        if (docfile[0].files.length === 0) {
+            alert('Please select a file to upload.');
+            return;
+        }
+        if (localStorage.getItem('user_id') === null || localStorage.getItem('user_id') === 'null') {
+            alert('Please login to upload a document.');
+            return;
+        }
         $.ajax({
-            url: 'http://34.207.190.195/temarisync/api/v1/documents/upload/' + user_id,
-            method: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
+            url: 'http://34.207.190.195/temarisync/api/v1/courses/',
+            method: 'GET',
             success: function (response) {
-                alert('Your form has been sent successfully.');
+                var validCourse = false;
+                $.each(response, function (i, crs) {
+                    if (crs.name.toLowerCase() === course.toLowerCase()) {
+                        course = crs.id;
+                        validCourse = true;
+                        return false; // Exit the loop early since a match is found
+                    }
+                });
+                if (!validCourse) {
+                    alert('Please enter a valid course.');
+                    console.error(crs.name);
+                    return;
+                }
+        
+                formData.append('title', title);
+                formData.append('description', discription);
+                formData.append('tags', tags);
+                formData.append('course_id', course);
+                formData.append('coAuthor', coAuthor);
+                formData.append('category', category);
+                formData.append('author_comment', $('#author_comment').val());
+                formData.append('stud_id', localStorage.getItem('user_id'));
+                formData.append('file', docfile[0].files[0]);
+        
+                $.ajax({
+                    url: 'http://34.207.190.195/temarisync/api/v1/documents/upload/' + user_id,
+                    method: 'POST',
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function (response) {
+                        alert('Your form has been sent successfully.');
+                    },
+                    error: function (xhr, status, error) {
+                        alert('Your form was not sent successfully.');
+                        console.error(error);
+                    }
+                });
             },
             error: function (xhr, status, error) {
-                alert('Your form was not sent successfully.');
                 console.error(error);
+                exit()
             }
         });
+        
     });
 });
