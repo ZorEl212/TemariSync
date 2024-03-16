bf48bf99f68bc7641ac140695c97866c1d164597
ZorEl212 <yabsirad212@gmail.com>
2024-03-16 00:46:37 -0700

    General: Scripts to automate app start

---
+++ run-api.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+export  TEMP_DOC_DIR='/home/ubuntu/TemariSync/temp/'
+export DOC_DIR='/home/ubuntu/TemariSync/docs/'
+export RETURN_DIR='/home/ubuntu/TemariSync/return/'
+export TS_ENV='dev'
+export TS_DB='ts_dev_db'
+export TS_DB_USER='ts_dev'
+export TS_DB_PWD='ts_dev_pwd'
+export TS_DB_HOST='localhost'
+export STORAGE_TYPE='db'
+
+python3 -m api.v1.app
+
