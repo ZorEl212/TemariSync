1264ad222f4acc6a4aac2264a68848a9572c6f70
ZorEl212 <yabsirad212@gmail.com>
2024-03-20 04:37:42 -0700

    API: create a route for logging out a user Frontend: Create logout button and it's functionality

--- api/v1/views/auth.py
+++ api/v1/views/auth.py
@@ -16,11 +16,11 @@ def login():
     students = storage.all('Student')
     for student in students.values():
         if ((student.email == email or student.school_id == email) and student.password == password):
-            login_user(student)
+            login_user(student, force=True)
             return jsonify(student.to_dict())
     return make_response(jsonify({"error": "unauthorized"}), 401)
 
-@app_views.route('/logout', strict_slashes=False, methods=['POST'])
+@app_views.route('/logout', strict_slashes=False, methods=['GET'])
 def logout():
     logout_user()
     return jsonify({"logout": "success"})
--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -1,13 +1,37 @@
 $(() => {
-    const $assignments = $('#assignments');
-    const $projects = $('#projects');
-    const $materials = $('#materials');
-    $assignments.attr('href', 'docs?category=assignment');
-    $projects.attr('href', 'docs?category=project');
-    $materials.attr('href', 'docs?category=material');
+  $.ajax({
+    url: "https://yeab.tech/temarisync/api/v1/checkuser",
+    method: "GET",
+    success: function (response, status, xhr) {
+      if (xhr.status === 200) {
+        $("#user").text(response.name);
+      } else {
+        window.location.href = "login";
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error(error);
+    },
+  });
 
-})
-const user_id = '182414ec-63b4-4e70-b727-51cddbccb55a';
-if (!localStorage.getItem('user_id') || localStorage.getItem('user_id') === 'null'){
-    localStorage.setItem('user_id', user_id);
-}
+  const $assignments = $("#assignments");
+  const $projects = $("#projects");
+  const $materials = $("#materials");
+  $assignments.attr("href", "docs?category=assignment");
+  $projects.attr("href", "docs?category=project");
+  $materials.attr("href", "docs?category=material");
+
+  $("#logout").click(() => {
+    $.ajax({
+      url: "https://yeab.tech/temarisync/api/v1/logout",
+      method: "GET",
+      success: function (response) {
+        localStorage.removeItem("user_id");
+        window.location.href = "login";
+      },
+      error: function (xhr, status, error) {
+        console.error(error);
+      },
+    });
+  });
+});
